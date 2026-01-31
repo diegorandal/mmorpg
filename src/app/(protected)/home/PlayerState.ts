aaabb64d4da0e4fedcc0a97296712623d4356f29
Diego <diegorandal@gmail.com>
2026-01-31 18:23:24 -0300

    sape

--- src/app/(protected)/home/PlayerState.ts
+++ src/app/(protected)/home/PlayerState.ts
@@ -1,8 +1,10 @@
 import { Schema, type } from '@colyseus/schema';
 
 export class PlayerState extends Schema {
-    @type('string') name = '';
-    @type('number') x = 0;
-    @type('number') y = 0;
-    @type('string') lastMessage = '';
+    @type("string") name: string = "";
+    @type("number") x: number = 100;
+    @type("number") y: number = 100;
+    @type("number") hp: number = 100;
+    @type("number") level: number = 1;
+    @type("string") lastMessage: string = "";
 }
