ce78e54c79750a5b08c190e12adfbacd1c614e74
Diego <diegorandal@gmail.com>
2026-02-02 18:34:09 -0300

    sape

--- src/app/(protected)/home/PlayerState.ts
+++ src/app/(protected)/home/PlayerState.ts
@@ -1,15 +1,16 @@
-import { Schema, type, MapSchema } from '@colyseus/schema';
-
+import { Schema, type, MapSchema } from "@colyseus/schema";
 
 export class Player extends Schema {
     @type("string") name: string = "";
-    @type("number") x: number = 100;
-    @type("number") y: number = 100;
-    @type("number") hp: number = 100;
-    @type("number") level: number = 1;
+    @type("number") x: number = 0;
+    @type("number") y: number = 0;
+    @type("number") hp: number = 0;
+    @type("number") level: number = 0;
     @type("string") lastMessage: string = "";
 }
 
 export class MyRoomState extends Schema {
+
     @type({ map: Player }) players = new MapSchema<Player>();
+
 }
