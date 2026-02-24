fe62399ee0c29e2029613d0619f9de168c5c3c8a
Diego <diegorandal@gmail.com>
2026-02-23 21:11:49 -0300

    sape

--- src/app/(protected)/game/scenes/systems/PlayerVisualSystem.ts
+++ src/app/(protected)/game/scenes/systems/PlayerVisualSystem.ts
@@ -132,14 +132,14 @@ export class PlayerVisualSystem {
         console.log('llega hasta aca');
 
         const damageLabel = this.scene.add.text(
-            entity.x, entity.y - 20,
+            entity.sprite.x, entity.sprite.y - 20,
             'def',
             { fontSize: "20px", color: "#003cff", fontStyle: "bold", stroke: "#000000", strokeThickness: 4 }
         ).setOrigin(0.5).setDepth(entity.sprite.depth);
 
         this.scene.tweens.add({
             targets: damageLabel,
-            y: entity.y - 80,
+            y: entity.sprite.y - 80,
             alpha: 0,
             duration: 750,
             ease: "Cubic.out",
