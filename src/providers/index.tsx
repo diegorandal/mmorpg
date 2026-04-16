db6086dbf8c4ea8f196a4e21fc56b6cac90bd163
Diego <diegorandal@gmail.com>
2026-04-15 23:41:55 -0300

    add flag mode

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -16,7 +16,7 @@ interface ClientProvidersProps {
   session: Session | null; // Use the appropriate type for session from next-auth
 }
 
-/*
+
 export default function ClientProviders({
   children,
   session,
@@ -30,8 +30,8 @@ export default function ClientProviders({
 
 
 }
-*/
 
+/*
 export default function ClientProviders({
   children,
   session,
@@ -44,4 +44,4 @@ export default function ClientProviders({
     </ErudaProvider>
   );
 }
-
+*/
