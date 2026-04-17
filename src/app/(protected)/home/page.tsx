b5544792df069908903ac50b8ec9de50cc1f8c88
Diego <diegorandal@gmail.com>
2026-04-16 21:24:58 -0300

    add flag mode

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -259,7 +259,7 @@ export default function Home() {
 
     // ======================================== SERVER FLAG ===================================
 
-    if (roomName == 'flag_room') {
+    if (roomName == 'flag_room' && playerWallet == '0x10fed80b87407320cfb2affbd68be78868937a6e') {
 
       try {
 
